30f418049d39af433962cea8c0d5d0d36e6f9b5b
vlevashov-dot <y.v.levashov@gmail.com>
2026-03-02 11:04:35 +0300

    ADC V1

---
+++ Get/get-adc/r2r_adc.py
@@ -0,0 +1,52 @@
+import RPi.GPIO as GPIO 
+import time
+class R2R_ADC:
+    def __init__(self, dynamic_range, compare_time = 0.01, verbose = False):
+        self.dynamic_range = dynamic_range
+        self.verbose = verbose
+        self.comare_time = compare_time 
+
+        self.bits_gpio = [26, 20, 19, 16, 13, 12, 25, 11]
+        self.comp_gpio = 21
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.bits_gpio, GPIO.OUT, initial = 0)
+        GPIO.setup(self.comp_gpio, GPIO.IN)
+
+    def deinit(self):
+        GPIO.output(self.bits_gpio,0)
+        GPIO.cleanup()
+
+    def number_to_dac(self,number):
+        out =  [int(i) for i in bin(number)[2:].zfill(8)]
+        GPIO.output(self.bits_gpio, out)
+    
+    def sequential_counting_adc(self):
+        for value in range(256):
+            signal = self.number_to_dac(value)
+
+            voltage = (value / 255) * (self.dynamic_range)
+            time.sleep(self.comare_time)
+            comparator_value = GPIO.input(self.comp_gpio)
+            if comparator_value == 1: 
+                print("ADC value = {:^3} -> {}".format(value,signal))
+                return (voltage)
+
+
+            if value == 255 and comparator_value!=1:
+                return (voltage)
+
+    def det_sc_voltage(self):
+        vlt = self.sequential_counting_adc(self)
+        return (vlt)
+        
+
+if __name__ == "__main__":
+    try:
+        adc = R2R_ADC(3.183,0.01,True)
+        while True:
+            voltage = adc.sequential_counting_adc()
+            print(voltage)
+
+    finally:
+        adc.deinit()
